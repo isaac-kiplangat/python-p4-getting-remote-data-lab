68578483cf658b1c5ccc4a04ddefba0a49fefdc8
5000vw <isaackiplangat1999@gmail.com>
2024-01-10 10:40:04 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,18 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
+        self.response = requests.get(url)
 
     def get_response_body(self):
-        pass
+        return self.response.content
 
     def load_json(self):
-        pass
+        try:
+            json_content = self.response.json()
+            return json_content
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            return None  
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
